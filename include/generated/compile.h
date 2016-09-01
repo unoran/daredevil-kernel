c9cc45a63e81e1e847e52f802e41d841aff2646a
Rohan Taneja <rohantaneja63@gmail.com>
2016-09-01 19:28:07 +0530

    dare-ify: upstream 3.10.103 and new selinux security patches

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 2 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#2 SMP PREEMPT Thu Sep 1 19:13:04 IST 2016"
+#define LINUX_COMPILE_BY "rohantaneja"
+#define LINUX_COMPILE_HOST "daredevil"
+#define LINUX_COMPILER "gcc version 4.9 20150123 (prerelease) (GCC) "
